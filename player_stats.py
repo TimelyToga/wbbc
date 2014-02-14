d97f0da1cf1d069fb17d31c7b79a2e041283a059
Timothy Blumberg <tblumberg39@gmail.com>
2014-02-14 00:25:07 -0500

    Cleaned up the earlier file version

--- player_stats.py
+++ player_stats.py
@@ -4,6 +4,7 @@ import urllib
 html_doc = urllib.urlopen('http://espn.go.com/mens-college-basketball/player/gamelog/_/id/66562/jabari-parker').read()
 
 soup = BeautifulSoup(html_doc)
+table = soup.find("table", attrs={"class":"tablehead"})
 
 rows = soup.findAll("tr", {'class':['evenrow', 'oddrow']})
 
@@ -12,11 +13,11 @@ records = []
 for row in rows:
 	game = {}
 	game['team'] = row.find("li", {"class" : "team-name"}).text
-	game['result'] = row.find("span", {"class" : ['greenfont', 'redfont']}).text
+	game['result'] = row.find("span", {"class" : ['greenfont', 'redfont']})
 	rowStats = [] # DATE	OPP	RESULT	MIN	FGM-FGA	FG%	3PM-3PA	3P%	FTM-FTA	FT%	REB	AST	BLK	STL	PF	TO	PTS
-	rowStats = row.find("td", {'span' : "text-align: right;"})
-	game['min'] = rowStats
+	#rowStats = row.find("td", {'span' : "text-align: right;"})
+	#game['min'] = rowStats
 	records.append(game)
 
-for k in records:
-	print (records[records.index(k)])
+#for k in records:
+	#print (records[records.index(k)])

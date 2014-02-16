eeb2395e16ba759ff97d330017c2adb0a27fc6de
Timothy Blumberg <tblumberg39@gmail.com>
2014-02-16 01:53:48 -0500

    Added the ability to control whether the output in the console is verbose or not with a '-v' after the program name. Very fancy. Added more explanatory comments.

--- player_stats2.py
+++ player_stats2.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 import csv
+import argparse
 
 # This method scrapes the data from a certain player's 'gamelog'
 # page. It outputs the data to the correct format.
@@ -19,41 +20,57 @@ def playerGet(url):
 			dataset.append(data)
 		datasets.append(dataset)
 
-	# for k in datasets:
-	# 	print (datasets[datasets.index(k)])
+	global args
+	if(args.v):
+		for k in datasets:
+			print (datasets[datasets.index(k)])
 
 	### TODO: Insert the new player's data into a database
 
 # This method cycles through all the players on a team roster page
 # and then calls the playerGet() for each of those players
 def teamGet(url):
+	print "working on" + url[30:]
 	team_url = urllib.urlopen(url).read()
 	teamSoup = BeautifulSoup(team_url)
 	playerTable = teamSoup.find("table", attrs={"class":"tablehead"})
 
 	for player in playerTable.find_all("tr")[1:]:
-		playerPage_url = str(player.find('a').get('href'))
+		playerPage_url = str(player.find('a').get('href')) # Must cast to string, as the BS4 works soley with unicode objects
 		playerPage = urllib.urlopen(playerPage_url)
 		playerSoup = BeautifulSoup(playerPage)
-		reg = re.compile(r'Game Log')
+		reg = re.compile(r'Game Log') # The search query; only grabs li objects with this text in them
 		elements = [e for e in playerSoup.find_all('li') if reg.match(e.text)]
 		if len(elements) != 0:
+			# Formats the team url correctly
 			player_url = 'http://espn.go.com' + elements[0].a.get('href')
 			print player_url
 			playerGet(player_url)
 
+# Grabs and then loops through all the NCAA Men's Basketball teams, and then calls teamGet() for each
 def main():
+	cmdOptions() # allows output supression 
+
 	league_url = "http://espn.go.com/mens-college-basketball/teams"
 	leaguePage = urllib.urlopen(league_url)
 	leagueSoup = BeautifulSoup(leaguePage)
 
+	# Builds a list of team roster links as found on the above url
 	teamRosterList = []
-	reg = re.compile(r'Roster')
+	reg = re.compile(r'Roster') # The search condition; only grabs a objects with text equal to this
 	teamRosterList = [e for e in leagueSoup.find_all('a') if reg.match(e.text)]
 
+	# Gets stats for each team found
 	for team in teamRosterList:
 		baseRoster_url = 'http://espn.go.com/mens-college-basketball/team/roster/_/id/'
+		# Grabs only the numbers out of the link and then formats it correctly to be called with teamGet()
 		callingTeam_url = baseRoster_url + re.findall(r'\d+', team.get('href'))[0]
 		teamGet(callingTeam_url)
 
-main()
+def cmdOptions():
+	parser = argparse.ArgumentParser(description='Scrape player data from ESPN')
+	parser.add_argument('-v', action='store_true')
+	global args
+	args = parser.parse_args()
+
+main() ## MAIN METHOD CALL
